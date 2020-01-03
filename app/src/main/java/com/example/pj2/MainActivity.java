7f841203b9ff40aa109942226bd2e8c522654069
boingkiri <djfelrl11@kaist.ac.kr>
2020-01-03 22:51:53 +0900

    facebook login

--- app/src/main/java/com/example/pj2/MainActivity.java
+++ app/src/main/java/com/example/pj2/MainActivity.java
@@ -1,27 +1,42 @@
 package com.example.pj2;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
+import java.security.MessageDigest;
+import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
-    private TabLayout tabLayout;
-    private TabItem tab1, tab2, tab3;
-    public SectionPageAdapter pagerAdapter;
-    private FrameLayout frame;
-    private ViewPager mViewpager;
-    SectionPageAdapter adapter = new SectionPageAdapter(getSupportFragmentManager());
-
+    private CallbackManager mCallbackManager;
 
     @SuppressWarnings("ClickableViewAccessibility")
 
@@ -29,18 +44,53 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        tabLayout = findViewById(R.id.tabs);
-        tab1 = findViewById(R.id.tab1);
-        tab2 = findViewById(R.id.tab2);
-        tab3 = findViewById(R.id.tab3);
 
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission_group.CONTACTS}, 1);
 
+//        FacebookSdk.sdkInitialize(this.getApplicationContext());
+
+        mCallbackManager = CallbackManager.Factory.create();
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+//        if (isLoggedIn){
+//            Log.d("AAA",accessToken.toString());
+//            Log.d(LoginManager.getInstance().);
+//        }
+        LoginManager.getInstance().registerCallback(mCallbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Log.d("Success", "Login");
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(MainActivity.this, "Login Cancel", Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                });
+
+        setContentView(R.layout.activity_main);
+
+//        Button btn_fb_login = findViewById(R.id.btn_fb_login);
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+//        btn_fb_login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
 
 //        mViewpager = findViewById(R.id.frame);
-
+        getHashKey(getApplicationContext());
 //        tabLayout.setupWithViewPager(mViewpager);
 
 
@@ -51,6 +101,39 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
+    @Nullable
+    public static String getHashKey(Context context) {
+        final String TAG = "KeyHash";
+        String keyHash = null;
+        try {
+            PackageInfo info =
+                    context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                keyHash = new String(Base64.encode(md.digest(), 0));
+                Log.d(TAG, keyHash);
+
+            }
+
+        } catch (Exception e) {
+            Log.e("name not found", e.toString());
+        }
+
+        if (keyHash != null) {
+            return keyHash;
+        } else {
+            return null;
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
 
 
